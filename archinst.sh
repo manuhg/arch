1be9df7d2eca23d4be92e8d29a81f2e998ea631d
manuhg <manuhegdev@gmail.com>
2017-08-09 22:53:16 +0530

    some changes

--- archinst.sh
+++ archinst.sh
@@ -23,18 +23,25 @@ echo mvspherical > /etc/hostname
 nano /etc/hosts
 mkinitcpio -p linux
 passwd
-pacman -Syu intel-ucode grub efibootmgr os-prober zsh arduino wireless_tools wpa_supplicant wpa_actiond dialog sudo
-systemctl enable net-autowireless.service
+pacman -Syu intel-ucode grub efibootmgr os-prober 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --debug
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S dnsutils wireless_tools wpa_supplicant wpa_actiond dialog sudo zsh
+#systemctl enable net-autowireless.service
+exit
+reboot
 #nano /etc/pacman.conf
 #[multilib]
 #Include = /etc/pacman.d/mirrorlist
-groupadd spherical
+groupadd spherical 
+groupadd dialout
 useradd -m -g spherical -G users,dialout,wheel,storage,power -s /bin/zsh gk1000
 passwd gk1000
 visudo
-pacman -S alsa-utils alsamixer xorg-server xorg-xinit xorg-server-utils mesa
+# alsamixer
+pacman -S alsa-utils  xorg-server xorg-xinit xorg-apps mesa  
 #synaptics for laptop 
-pacman -S xf86-video-intel xf86-synaptics xf86-video-vesa
+pacman -S xf86-video-intel xf86-input-synaptics xf86-video-vesa
 #xf86-video-vesa is generic & fallback driver
 # kde5 plasma -sddm , gnome GDM , lightgdm -universal
 # pacman -Rcs => recursive remove
@@ -53,7 +60,7 @@ cd yaourt
 makepkg -si
 cd ..
 rm -dR package-query yaourt
-pacman -S hexedit  python   nitrogen  testdisk rng-tools steghide foremost exiv2 cryptsetup-bin whois geoip-bin virtualbox  nasm gcc g++ php qemu-utils geany vlc python-pip aircrack-ng build-essential automake libevent-dev libssl-dev ruby  nodejs npm gem  screenfetch synapse tty-clock pm-utils zsh python-tk
+pacman -S hexedit  python   nitrogen arduino  testdisk rng-tools steghide foremost exiv2 cryptsetup-bin whois geoip-bin virtualbox  nasm gcc g++ php qemu-utils geany vlc python-pip aircrack-ng build-essential automake libevent-dev libssl-dev ruby  nodejs npm gem  screenfetch synapse tty-clock pm-utils zsh python-tk
 yaourt -S visual-studio-code-oss google-chrome
 #gnome-tweak-tool thunar filezilla irssi
 #synaptic tasksel openjdk-7-* vlc-plugin-libde265 w3af p7zip-full 
