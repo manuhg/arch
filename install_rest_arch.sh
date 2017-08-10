f8329244192cad092efffc3d781ce85abb4d0dc3
manuhg <manuhegdev@gmail.com>
2017-08-10 05:07:34 +0000

    chages from lappy

--- install_rest_arch.sh
+++ install_rest_arch.sh
@@ -8,13 +8,18 @@ cd yaourt
 makepkg -si
 cd ..
 rm -dR package-query yaourt
-pacman -S hexedit  python   nitrogen arduino  testdisk rng-tools steghide foremost exiv2  whois geoip virtualbox  nasm gcc  php qemu geany vlc python-pip aircrack-ng  automake ruby  nodejs npm ruby-native-package-installer  screenfetch synapse zsh encfs docker
+pacman -S hexedit  python   nitrogen arduino  testdisk rng-tools \
+steghide foremost exiv2  whois geoip virtualbox  nasm gcc  php \
+qemu geany vlc python-pip aircrack-ng  automake ruby  nodejs npm \
+ruby-native-package-installer screenfetch synapse zsh encfs docker 
 yaourt -S visual-studio-code-oss google-chrome
+
 #cryptsetup-bin gnome-tweak-tool thunar filezilla irssi
 #synaptic tasksel openjdk-7-* vlc-plugin-libde265 w3af p7zip-full 
 #->atom  xampp #smartmontools
+
 npm -g install react react-dom webpack babel create-react-app
 
-DWM, i3, openbox, bspwm
+#DWM, i3, openbox, bspwm
 
 pip install matplotlib numpy 
