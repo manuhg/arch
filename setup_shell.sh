681bf55de84f5124b1c18532c52deedc125c16d7
manu-spherical <manuhegdev@gmail.com>
2018-02-11 23:19:36 +0530

    emacs dir

--- setup_shell.sh
+++ setup_shell.sh
@@ -8,19 +8,19 @@ rc="\033[0;31m"
 nc="\033[0m"
 bold="\033[1m"
 
-edirs=("$HOME/.local/share" "$HOME" "$HOME" "$HOME" "$HOME" "$HOME")
-edf=("gnome-shell" ".emacs.d/core" ".emacs.d/modules" ".emacs.d/personal" ".emacs.d/sample" ".emacs.d/savefile")
+edirs=("$HOME/.local/share" "$HOME" )
+edf=("gnome-shell" ".emacs.d")
 dconfs=("gnome/")
 # "pantheon/"
 fdirs=("$HOME" "$HOME/.oh-my-zsh/themes" "$HOME/.local/share/konsole" "$HOME/.local/share/konsole" \
        "$HOME/.config" "$HOME" "$HOME" "$HOME" \
        "$HOME/.config/nitrogen" "$HOME/.config/i3" "$HOME/.config/gtk-3.0" "$HOME/.config/gtk-3.0" \
-       "$HOME/.config" "/etc/clamav-unofficial-sigs" "$HOME" "$HOME/.emacs.d" "$HOME/.emacs.d" )
+       "$HOME/.config" "/etc/clamav-unofficial-sigs" "$HOME"  )
 
 files=(".zshrc" "gk1000.zsh-theme" "gk1000.profile" "Solarized.colorscheme" \
        "konsolerc" ".tmux.conf" ".Xresources" ".gitconfig"  \
        "bg-saved.cfg" "config" "bookmarks" "settings.ini" \
-       "numix-folders" "user.conf" ".emacs" ".emacs.desktop" "init.el" )
+       "numix-folders" "user.conf" ".emacs")
 if [  $# -eq 0 ]; then
     echo "Please enter an argument. backup or restore"
 
